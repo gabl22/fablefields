513f0663d576831b79301e733f86c658c9b7e8d3
gabl22 <gabl22ytmc@gmail.com>
2026-01-19 15:14:18 +0100

    update tooltip rendering

--- core/src/main/java/me/gabl/fablefields/screen/game/ToolTipHud.java
+++ core/src/main/java/me/gabl/fablefields/screen/game/ToolTipHud.java
@@ -3,13 +3,12 @@ package me.gabl.fablefields.screen.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import me.gabl.fablefields.asset.Asset;
 import me.gabl.fablefields.game.inventory.ItemType;
-import me.gabl.fablefields.game.inventory.item.GenericItems;
 import me.gabl.fablefields.screen.ui.Hud;
 import me.gabl.fablefields.screen.ui.UiSkin;
 import org.jetbrains.annotations.NotNull;
@@ -18,19 +17,22 @@ import org.jetbrains.annotations.Nullable;
 public class ToolTipHud extends Hud {
 
     private final Label label;
-    private final Group root;
+    private final Table root;
     private final Image itemImage;
     private final Image base;
 
     public ToolTipHud(SpriteBatch batch) {
         super(batch);
-        this.root = new Group();
+        this.root = new Table();
 
         Skin skin = UiSkin.skin();
-        this.label = new Label("!", skin, "background"); // need any symbol for correct label.height
+        this.label = new Label(null, skin, "background"); // need any symbol for correct label.height
         Texture baseTexture = Asset.manager.get(Asset.getTexture("ui/itemdisc.png"));
         this.base = new Image(baseTexture);
-        this.itemImage = new Image(GenericItems.WOOD.render()); // any item with item size
+        this.itemImage = new Image();
+        itemImage.setSize(16, 16);
+        label.setHeight(31);
+        label.setWrap(false);
         root.addActor(label);
         root.addActor(base);
         root.addActor(itemImage);
@@ -39,7 +41,8 @@ public class ToolTipHud extends Hud {
         itemImage.scaleBy(2f);
         itemImage.setPosition((base.getWidth() - itemImage.getWidth()) / 2 + 5,
                 (base.getHeight() - itemImage.getHeight()) / 2 + 5);
-        label.setPosition(base.getWidth() * base.getScaleX() - 12, (base.getHeight() * base.getScaleY() - label.getHeight()) / 2);
+        label.setPosition(base.getWidth() * base.getScaleX() - 12,
+                (base.getHeight() * base.getScaleY() - label.getHeight()) / 2);
         stage.addActor(root);
     }
 
@@ -52,9 +55,11 @@ public class ToolTipHud extends Hud {
         this.label.setText("  " + text);
         label.setWidth(label.getPrefWidth());
         itemImage.setDrawable(itemType.render());
+        root.pack();
     }
 
     public void updatePosition(@NotNull Vector2 screenPosition) {
+        //todo merge duplicate below/in ItemHoverHud
         Vector2 cursorPosition = stage.screenToStageCoordinates(screenPosition);
         float width = stage.getViewport().getWorldWidth();
         float height = stage.getViewport().getWorldHeight();
